8b326dacdb9f1944a4002e353a79055fe787888f
Azoller11 <azoller11@gmail.com>
2021-03-01 11:54:19 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace CST117Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_enterWeight = new System.Windows.Forms.TextBox();
+            this.lb_planets = new System.Windows.Forms.ListBox();
+            this.tb_finalWeight = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your weight on Earth:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your weight on:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tb_enterWeight
+            // 
+            this.tb_enterWeight.Location = new System.Drawing.Point(179, 57);
+            this.tb_enterWeight.Name = "tb_enterWeight";
+            this.tb_enterWeight.Size = new System.Drawing.Size(233, 20);
+            this.tb_enterWeight.TabIndex = 2;
+            // 
+            // lb_planets
+            // 
+            this.lb_planets.FormattingEnabled = true;
+            this.lb_planets.Location = new System.Drawing.Point(128, 97);
+            this.lb_planets.Name = "lb_planets";
+            this.lb_planets.Size = new System.Drawing.Size(80, 17);
+            this.lb_planets.TabIndex = 3;
+            this.lb_planets.SelectedIndexChanged += new System.EventHandler(this.lb_planets_SelectedIndexChanged);
+            // 
+            // tb_finalWeight
+            // 
+            this.tb_finalWeight.Location = new System.Drawing.Point(223, 95);
+            this.tb_finalWeight.Name = "tb_finalWeight";
+            this.tb_finalWeight.Size = new System.Drawing.Size(188, 20);
+            this.tb_finalWeight.TabIndex = 4;
+            this.tb_finalWeight.TextChanged += new System.EventHandler(this.tb_finalWeight_TextChanged);
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(41, 145);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(75, 23);
+            this.btn_convert.TabIndex = 5;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(419, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pounds (lbs)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pounds (lbs)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 437);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.tb_finalWeight);
+            this.Controls.Add(this.lb_planets);
+            this.Controls.Add(this.tb_enterWeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_enterWeight;
+        private System.Windows.Forms.ListBox lb_planets;
+        private System.Windows.Forms.TextBox tb_finalWeight;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST117Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lb_planets.Items.Add("Mars");
+            lb_planets.Items.Add("Venus");
+            lb_planets.Items.Add("Mercury");
+            lb_planets.Items.Add("Saturn");
+            lb_planets.Items.Add("Jupiter");
+            lb_planets.Items.Add("Moon");
+
+        }
+
+        private void lb_planets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_finalWeight_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            //Get weight inputed
+            double ew = 0;
+
+            try
+            {
+                ew = Double.Parse(tb_enterWeight.Text);
+            } catch (FormatException)
+            {
+                MessageBox.Show("Please enter your weight in pounds (lbs)");
+            }
+          
+             
+            if (lb_planets.SelectedIndex == 0)
+            {
+                ew *= 0.377; //Mars Conversion
+            } else if (lb_planets.SelectedIndex == 1)
+            {
+                ew *= 0.887; //Venus Conversion
+            } else if (lb_planets.SelectedIndex == 2)
+            {
+                ew *= 0.37; //Mercury Conversion
+            } else if (lb_planets.SelectedIndex == 3)
+            {
+                ew *= 1.044; //Saturn Conversion
+            } else if (lb_planets.SelectedIndex == 4)
+            {
+                ew *= 2.479; //Jupiter Conversion
+            } else if (lb_planets.SelectedIndex == 5)
+            {
+                ew *=  0.1622; //Moon Conversion
+            } else
+            {
+                MessageBox.Show("Please select a celestial body.");
+            }
+
+
+
+
+
+            tb_finalWeight.Text = String.Format("{0:0.000}", ew);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
